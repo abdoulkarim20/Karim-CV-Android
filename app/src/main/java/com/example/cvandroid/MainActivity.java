a206e9e54bb4ac244b6e33a615853a950dd624ec
jabbarDevOps <jabbar.devops@gmail.com>
2021-12-01 12:32:48 +0000

    realisation du menu

--- app/src/main/java/com/example/cvandroid/MainActivity.java
+++ app/src/main/java/com/example/cvandroid/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.cvandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items ,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView rigthicon=findViewById(R.id.right_icon);
+        TextView titelog=findViewById(R.id.toolbar_title);
+//        attribut icone gauche
+//        lefticon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "test clic gauche" , Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+//        attrubut icone droite
+        rigthicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "test clic droite" , Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
